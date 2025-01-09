3b771b0745b40afb74b9ccfe0a5dde56efb137c8
Cooper Mattern <cnamattern@gmail.com>
2025-01-08 18:13:19 -0800

    inital commit

---
+++ p1/trace.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <pcap.h>
+
+#include "utils.h"
+#include "trace.h"
+#include "checksum.h"
+
+int main(int argc, char* argv[]){
+	return EXIT_SUCCESS
+}
---
+++ p1/utils.h
@@ -0,0 +1,7 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAIL 1
+
+#endif

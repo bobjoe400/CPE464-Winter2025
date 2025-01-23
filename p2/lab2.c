88a8522b75c5106f224819cb38b21306ac9fca34
Cooper Mattern <cnamattern@gmail.com>
2025-01-23 15:03:21 -0800

    lab 2 part 3 done

--- p2/lab2.c
+++ p2/lab2.c
@@ -18,19 +18,20 @@ void print_packet_hex(uint8_t* buff, int len){
 }
 
 int sendPDU(int clientSocket, uint8_t* dataBuffer, int lengthOfData){
-    int pdu_len = htonl(lengthOfData + PDU_HDR_LEN);
+    int pdu_len = lengthOfData + PDU_HDR_LEN;
 
     uint8_t app_pdu[pdu_len + 1];
+    uint16_t hdr_pdu_len = htons(pdu_len);
 
     memset(app_pdu, 0, pdu_len + 1);
-    memcpy(app_pdu, &pdu_len, PDU_HDR_LEN);
+    memcpy(app_pdu, &hdr_pdu_len, PDU_HDR_LEN);
     memcpy(&app_pdu[PDU_HDR_LEN], dataBuffer, lengthOfData);
 
     int bytes_sent= send(clientSocket, app_pdu, pdu_len, 0) ;
 
     if(bytes_sent < 0){
         printf("Error sending packet to socket: %d\n", clientSocket);
-        printf("\tPacket:");print_packet_hex(app_pdu, pdu_len);
+        printf("Packet:\n"); print_packet_hex(app_pdu, pdu_len);
 
         perror("lab2::sendPDU()");
         exit(-1);
@@ -55,7 +56,7 @@ int recvPDU(int socketNumber, uint8_t* dataBuffer, int bufferSize){
         return 0;
     }
 
-    pdu_len = ntohl(pdu_len);
+    pdu_len = ntohs(pdu_len);
     
     if (pdu_len - PDU_HDR_LEN > bufferSize){
         printf("dataBuffer too small! buff size: %d pdu len: %d\n", bufferSize, pdu_len);
@@ -64,7 +65,7 @@ int recvPDU(int socketNumber, uint8_t* dataBuffer, int bufferSize){
 
     payload_len = pdu_len - PDU_HDR_LEN;
 
-    bytes_received = recv(socketNumber, &dataBuffer, payload_len, MSG_WAITALL);
+    bytes_received = recv(socketNumber, dataBuffer, payload_len, MSG_WAITALL);
 
     if(bytes_received < 0){
         printf("Error reciving packet from socket: %d\n", socketNumber);
